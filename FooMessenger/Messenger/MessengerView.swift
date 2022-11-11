fbabdb495083b8cb98a616eb35b7ed6549fead54
Hollis Kwan <holliskwan.ios@gmail.com>
2022-11-10 19:32:02 -0800

    Updated

---
+++ FooMessenger/Messenger/MessengerView.swift
@@ -0,0 +1,22 @@
+//
+//  MessengerView.swift
+//  FooMessenger
+//
+//  Created by Hollis Kwan on 10/9/22.
+//
+
+import Foundation
+import UIKit
+
+class MessengerView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .white
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
